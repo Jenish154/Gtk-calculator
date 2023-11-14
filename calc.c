4f2fb6ea679a7eff81f895005cfdf7cd4e4f9e6e
jenish.potsangbam215 <jenish.potsangbam215@outlook.com>
2023-11-14 23:03:03 +0530

    Added functionality to the number buttons

--- calc.c
+++ calc.c
@@ -1,13 +1,25 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 
 typedef struct
 {
     GObject *entry;
-    int num;
+    char num;
 }InpData;
 
 
-void calculate(GtkWidget *widget, gpointer data)
+void num_button_handler(GtkWidget *widget, gpointer data)
+{
+    InpData *inp_data = data;
+    GtkEntry *entry = GTK_ENTRY(inp_data->entry);
+
+    //select the text in the entry append the new one and set that one as the new text
+    const gchar *text = gtk_entry_get_text(entry);
+    gchar *new_text = g_strconcat(text, &(inp_data->num), NULL);
+    gtk_entry_set_text(entry, new_text);
+}
+
+void compute(GtkWidget *widget, gpointer data)
 {
 
 }
@@ -35,9 +47,33 @@ int main(int argc, char **argv)
     entry = gtk_builder_get_object(builder, "inp");
     
     button = gtk_builder_get_object(builder, "button_calc");
-    g_signal_connect(button, "clicked", G_CALLBACK(calculate), NULL);
-
+    g_signal_connect(button, "clicked", G_CALLBACK(compute), NULL);
+    /*
+    InpData data = 
+    {
+        .entry=entry,
+        .num='1'
+    };
     button = gtk_builder_get_object(builder, "button_1");
+    g_signal_connect(button, "clicked", G_CALLBACK(num_button_handler), &data);
+
+    data.num = '2';
+    button = gtk_builder_get_object(builder, "button_2");
+    g_signal_connect(button, "clicked", G_CALLBACK(num_button_handler), &data);
+    */
+    char buffer[9];
+
+    for (char i = '0'; i <= '9'; i++) 
+    {
+        //instantiate new struct data handled by gtk
+        InpData *data = g_new(InpData, 1);
+        data->entry = entry;
+        data->num = i;
+
+        sprintf(buffer, "button_%c", i);
+        button = gtk_builder_get_object(builder, buffer);
+        g_signal_connect(button, "clicked", G_CALLBACK(num_button_handler), data);
+    }
 
     gtk_main();
 
